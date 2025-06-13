cca316e9a0e5b7a23e8682d776eb694268c49a90
Pratik Dev <pratikdevofficial1@gmail.com>
2025-06-13 14:00:00 +0600

    initialized shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 
-import "./App.css";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
@@ -9,14 +8,13 @@ function App() {
 
 	return (
 		<>
-			<div>
+			<div className="bg-red-500">
 				<a
 					href="https://vite.dev"
 					target="_blank"
 				>
 					<img
 						src={viteLogo}
-						className="logo"
 						alt="Vite logo"
 					/>
 				</a>
@@ -32,7 +30,7 @@ function App() {
 				</a>
 			</div>
 			<h1>Vite + React</h1>
-			<div className="card">
+			<div>
 				<button onClick={() => setCount((count) => count + 1)}>
 					count is {count}
 				</button>
@@ -40,9 +38,7 @@ function App() {
 					Edit <code>src/App.tsx</code> and save to test HMR
 				</p>
 			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
+			<p>Click on the Vite and React logos to learn more</p>
 		</>
 	);
 }
